250c99c09793b8aa02c91bb0989d9337519052d8
mateus <mateuslourenco55@outlook.com>
2021-12-19 14:35:29 -0300

    Produção de Código Testável
    close #32

--- libpythonpro/tests/tests_spam/test_usuarios.py
+++ libpythonpro/tests/tests_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Mateus')
+    usuario = Usuario(nome='Mateus', email='mateuslourenco55@outlook.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Mateus'), Usuario(nome='Rafael')]
+    usuarios = [
+        Usuario(nome='Mateus', email='mateuslourenco55@outlook.com'),
+        Usuario(nome='Rafael', email='mateusalourenco@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
