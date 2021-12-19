a65f0c636b1a0d4b740e34b9160f87b1adbd14d6
mateus <mateuslourenco55@outlook.com>
2021-12-19 11:46:35 -0300

    Emulando Teste com Banco de Dados

---
+++ libpythonpro/tests/tests_spam/test_usuarios.py
@@ -0,0 +1,21 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Mateus')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_listar_usuarios():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Mateus'), Usuario(nome='Mateus')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
