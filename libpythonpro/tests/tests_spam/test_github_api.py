d5526c6b300ef40318cee9a5a30256fcd96d0f3b
mateus <mateuslourenco55@outlook.com>
2021-12-19 15:39:09 -0300

    Módulo como Objeto
    close #38

---
+++ libpythonpro/tests/tests_spam/test_github_api.py
@@ -0,0 +1,15 @@
+from unittest.mock import Mock
+
+from libpythonpro import github_api
+
+
+def test_buscar_avatar():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {
+        'login': 'mateuslourenco',
+        'id': 19513199,
+        'avatar_url': 'https://avatars.githubusercontent.com/u/19513199?v=4'
+    }
+    github_api.requests.get = Mock(return_value=resp_mock)
+    url = github_api.buscar_avatar('mateuslourenco')
+    assert 'https://avatars.githubusercontent.com/u/19513199?v=4' == url
