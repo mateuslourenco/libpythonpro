3b648fa4fb534d59eb90a6245e6ecf0f442390ff
mateus <mateuslourenco55@outlook.com>
2021-12-19 14:35:29 -0300

    Produção de Código Testável
    close #32

--- libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+import pytest
+
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Mateus', email='mateuslourenco55@outlook.com'),
+            Usuario(nome='Rafael', email='mateuslourenco55@outlook.com')
+        ],
+        [
+            Usuario(nome='Mateus', email='mateuslourenco55@outlook.com')
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'mateuslourenco55@outlook.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
